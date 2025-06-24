6f8e150796e104408059b474a0bf3f8d98ccf9ab
RonaldOellers <Ronald.j.w.oellers@gmail.com>
2025-06-24 22:29:47 +0200

    fix help argument

--- af3builder/build.py
+++ af3builder/build.py
@@ -4,13 +4,13 @@ from .core import AF3Builder
 from .exceptions import AF3Error
 
 @click.command()
-@click.argument("input_file", type=click.Path(exists=True), help="Provide input yaml or tsv file")
+@click.argument("input_file", type=click.Path(exists=True))
 @click.option("-em", "--email", help="Email for NCBI access")
 @click.option("-o", "--output", help="Output FASTA file", default="af3_input.fasta")
 @click.option("-v", "--verbose", is_flag=True, help="Verbose output for debugging")
 
 def build(input_file, email, output, verbose):
-    """Convert biological data to AlphaFold3 input FASTA"""
+    """Convert biological data to AlphaFold3 input FASTA. Provide input yaml or tsv file."""
     try:
         builder = AF3Builder(email=email)
         builder.build(input_file, output, verbose)
